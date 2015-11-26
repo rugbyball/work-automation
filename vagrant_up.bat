bb2b6ce09ebc242e54c4b79191522a80398dba3a
rugbyball <tzeng.icr915@gmail.com>
2015-11-26 10:15:11 +0800

    vagrant_up.bat

---
+++ vagrant_up.bat
@@ -0,0 +1,11 @@
+d:
+cd d:\vagrant_droi\control-machine-vm
+vagrant up
+cd d:\vagrant_droi\voltdb-cluster-vm
+vagrant up
+
+
+start "" "C:\Program Files\Git\git-bash.exe" --cd-to-home
+start "" "C:\Program Files\Git\git-bash.exe" --cd-to-home
+
+C:\Windows\System32\cmd.exe /k "cd d:\ & D: & ipython notebook %python_droi inline"
